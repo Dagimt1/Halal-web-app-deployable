d6570e69350c5ac47a01fb5d9e763c77c1bd21df
Dagim Jalleta <dagimteriessa@gmail.com>
2024-10-27 19:27:06 -0500

    added route handles for getting a restaurnat and set up a skeleton code for posting a restaurant.

--- server/server.js
+++ server/server.js
@@ -1,12 +1,31 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
 
-app.get("/getRestaurants", (req, res) => {
-    res.json({
-        status: ""
-    })
+app.use((req, res, next) => {
+  console.log("yeah our middleware")
+  next()
+})
+
+app.get("/api/v1/restaurants", (req, res) => {
+  console.log("route handler ran")
+  res.status(200).json({
+    status: "success",
+    data: {
+      restaurant: ["mcdonalds", "wendys"],
+    }
+    
+  });
+});
+
+app.get("/api/v1/restaurants/:restaurant_id", (req, res) => {
+  console.log(req.params)
+})
+
+app.post("/api/v1/restaurants", (req, res) => {
+  console.log(req.body)
+
 })
 
 const port = process.env.PORT || 3001;
