4ce0322c2600ec970855e9355e0f1b3b1a6e4c1f
Dagim Jalleta <dagimteriessa@gmail.com>
2024-12-10 19:25:16 -0600

    changed the base url

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -1,5 +1,5 @@
-import axios from "axios"
+import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:3001/api/v1/restaurants",
-})
+    baseURL: "https://halal-web-app-deployable.onrender.com/api/v1/restaurants",
+});
